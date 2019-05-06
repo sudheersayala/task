fe72728ee0e3309c0a6f837a924ad88885ea9599
sudheersayala <50326219+sudheersayala@users.noreply.github.com>
2019-05-06 18:12:01 +0530

    Add files via upload

---
+++ enter_input.py
@@ -0,0 +1,3 @@
+name = input('what is your name:')
+age = input('what is your age: ')
+print ('my name is ' + name , '\n my age is '+ age)
---
+++ odd_or_even.py
@@ -0,0 +1,8 @@
+num = int(input('enter a number:'))
+n = num%2
+if n == 0:
+    print('it is an even number')
+elif n!= 0:
+    print('it is an odd number')
+else :
+    print('invalid input')
