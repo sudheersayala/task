6ab0d14ce141d414d7293a30130dacae449eb56a
sudheersayala <50326219+sudheersayala@users.noreply.github.com>
2019-05-07 15:18:42 +0530

    NEW REPO

---
+++ lists.py
@@ -0,0 +1,14 @@
+def  less_thn(lis):
+
+    output = []
+    for num in lis:
+      if num < 10:
+        output.append(num)
+
+    return output
+
+ a =  less_thn ([5, 4, 6, 2, 78, 99, 32, 3]
+
+
+ print(a)
+
